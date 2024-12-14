bd490ab9c3d520d1d5f47be0d73517d8c9895c38
JoseLpuerto <jpuerto88@yahoo.com>
2024-12-13 18:40:00 -0600

    se agregan funciones de getUser y createUser

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 // end point https://reqres.in/api/users
 
-const getEmpleado = ()=>{
-    $.getJSON("https://reqres.in/api/users", (data)=>{
-        console.log(data.data)
+const getUser = ()=>{
+    return new Promise((resolve, reject)=>{
+        $.getJSON("https://reqres.in/api/users", (data)=>{
+    const User = data.data;
+    resolve(User)
+    }).fail((err)=>{
+        reject(err)
+    })
+    
     })
 }
 
-getEmpleado()
+const createEmpleado=(User)=>{
+    return `<div class="col-6 col-sm-4 col-md-4 col-xl-3 my-1">
+                <div class="card p-2 h-100 text-wrap" style="width: auto;">
+                    <img src="${User.avatar}" class="card-img-top" alt="user-image" style="width: 75px;>
+                    <div class="card-body d-flex align-items-end">
+                      <h6 class="card-title text-capitalize">${User.first_name} ${User.last_name}</h6>
+                      <h8>Email: <span style="font-weight: normal;">${User.email}</span></h8>
+                      <p style="text-align: end;"><strong>${User.id}</strong></p>
+                    </div>
+                  </div>
+            </div>`;
+}
+
+$( async function () {
+    const Users = await getUser();
+
+    Users.forEach( (User) => {
+        const UserHTML= createEmpleado(User)
+        $("#emp-container").append(UserHTML)        
+    });
+    
+})
